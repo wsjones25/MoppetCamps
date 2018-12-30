f3f869e9da7af539fe360c4b574d6d5b545771f6
Will Jones <wsjones1101@gmail.com>
2018-12-30 18:44:48 +0700

    added user devise model

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,18 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+ 
   root to:'pages#home'
+
+  devise_for :users, controllers: {
+    sessions:           "users/sessions",
+    passwords:          "users/passwords",
+    registrations:      "users/registrations",
+    confirmations:      "users/confirmations",
+    unlocks:            "users/unlocks"
+  }
+
+  get 'about', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
+  get 'login', to: 'pages#login'
+  get 'sign_up', to: 'pages#sign_up'
+  
 end
